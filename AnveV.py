c8b23f697c927be9773280417e7d258ead92b9b5
AndeV <138633572+AndyDav6@users.noreply.github.com>
2024-04-19 17:57:25 -0500

    Add files via upload

---
+++ AnveV.py
@@ -0,0 +1,63 @@
+# https://www.python.org/
+
+#Este es un comentario
+
+"""
+Este es un 
+comentario en 
+varias lineas con comillas
+"""
+
+'''
+Y este es otra forma 
+de escribir comentario 
+en varias lineas con comillas simples
+'''
+
+# La siguiente linea es un varible
+my_name = "Andy"
+my_favorite_color = "#09f"
+
+"""
+Python no tiene constantes pero por convención 
+si escribimos una variable en mayusculas 
+lo que representamos es una varible constante
+"""
+MY_CONSTANT = "Mi constante :D"
+
+#Tipos de datos primitivos de Python
+
+#INT: es un tipo de dato para representar numeros enteros
+mi_numero = 115 
+
+#FLOAT: es un tipo de dato para reprentar numeros decimales
+mi_decimal = 93.5
+
+"""BOOL: es un tipo de dato que solo atiende a logica boolena 
+es decir a True o False"""
+my_boolt = True
+my_boolf = False
+
+#STRING: es una secuecia de caracteres que se sua para representar el texto
+my_string = "Esto es un string"
+my_other_string = 'String con comillas simples'
+
+#Para mostrar informacion en el terminal en python se usa print()
+print("¡Hola, Python!")
+
+#Ahora mostremos en el terminal las variables, la constante y los tipos de datos
+
+#LAS VARIABLES
+print(my_name)
+print(my_favorite_color)
+
+#LA CONSTANTE
+print(MY_CONSTANT)
+
+#Tipo de datos
+print(mi_numero)
+print(mi_decimal)
+print(my_boolt)
+print(my_boolf)
+print(my_string)
+print(my_other_string)
