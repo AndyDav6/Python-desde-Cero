2bd57efd9594c43ecb256fd4c5b6e9c47567730f
AndyDav6 <138633572+AndyDav6@users.noreply.github.com>
2024-07-18 19:05:22 -0500

    py 1.001

---
+++ src basico/Tipdat.py
@@ -0,0 +1,24 @@
+# TIPOS DE DATOS (númericos, booleanos, variables strings)
+
+#Variable
+Color_favorito = "#09f"
+CONST = "ESTO ES UNA VARIABLE CONSTANTE"
+
+# Tipo de dato numerico
+num_entero = 15
+print(num_entero)
+
+num_float = 10.5
+print(num_float)
+
+num_complejo = 1 +2j
+print(num_complejo)
+
+# Tipo de dato booleano
+mi_booleano = True
+mi_bool = False
+print(mi_booleano)
+
+# Tipo de dato String o cadena de texto 
+mi_striing = "¡Hola mundo!"
+print(mi_striing)
