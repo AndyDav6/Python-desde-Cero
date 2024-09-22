7fd6dd3f32f90933cb42ef25734658a2340be7ab
AndyDav6 <138633572+AndyDav6@users.noreply.github.com>
2024-09-21 19:58:38 -0500

    py 1.004

--- src_01basico/02-Funciones.py
+++ src_01basico/02-Funciones.py
@@ -14,6 +14,42 @@ de la funcion'''
 Esto se hace escribiendo el nombre de la funcion seguido de 
 parentesis () y proporcionando cualquier parametro necesario'''
 
+# Funcion predefinida
+print("Me llamo Andy Namaja")
+
+# Funcion predefinida len() --> el "len" calcula la longitud de la lista.
+mi_lista = [1, 2, 3, 4]
+longitud = len(mi_lista)
+print(longitud)
+
+# Funciones definidas por el  usuario
+def sumar (a, b):
+    return a + b
+resultado = sumar(15, 4)
+print(resultado)
+
+# Funciones con argumentos por defecto
+def saludar(nombre = "bro"):
+    print(f"Hola, {nombre}")
+
+saludar()
+saludar("Dan")
+
+# Funciones anonimas (lambda)
+multiplicar = lambda x, y: x * y
+print(multiplicar(5,3))
+
+# Funciones con numeros variables de argumentos
+# *args: permite pasar un numero variable de argumento no nombrados
+def sumar_todos(*args):
+    return sum(args)
+print(sumar_todos(3, 6, 9 ,12))
+
+# **kwargs: permite pasar un numero variable de argumentos con nombre
+def imprimir_info(**kwargs):
+    for clave, valor in kwargs.items():
+        print(f"{clave}: {valor}")
+imprimir_info(Nombre="Gaby", Edad="20", Ciudad="Cuenca")
 
 # Defincion de una funcion
 def saludo ():
